e3b9c24a87ea84f5155aef16caec1ef34b0d4841
remi9023 <lch857953@gmail.com>
2025-03-16 13:26:31 +0900

    다이노 무리/ 텍스트 배치

--- Assets/02.Scripts/DinoPositionController.cs
+++ Assets/02.Scripts/DinoPositionController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class DinoPositionController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Transform raptors;
+    
+    public float radius = 1f; // 원의 반지름
+    public float ratio = 0.1f; // 배치 간격 비율 (작을수록 촘촘)
     void Update()
     {
-        
+        SetDinoPosition();
+    }
+    private void SetDinoPosition()
+    {
+        // 360도 각도 계산을 위한 각도 증가값
+        float angleStep = 360f / (raptors.childCount * ratio);
+        for (int i = 0; i < raptors.childCount; i++)
+        {
+            // 각 오브젝트의 배치 각도 계산
+            float angle = i * angleStep;
+            // 각도를 라디안으로 변환
+            float angleRad = angle * Mathf.Deg2Rad; // degree 를 radian으로 치환
+            // X와 Z 좌표를 원형으로 계산
+            float x = Mathf.Cos(angleRad) * radius;
+            float z = Mathf.Sin(angleRad) * radius;
+            // 새로운 위치로 자식 오브젝트를 위치시킴
+            raptors.GetChild(i).localPosition = new Vector3(x, 0, z);
+        }
     }
 }
+          
