5a8674e3ad6fbe41109ea7070af07298dd8dd2f7
remi9023 <lch857953@gmail.com>
2025-04-13 00:46:03 +0900

    도어체크

--- Assets/02.Scripts/DinoController.cs
+++ Assets/02.Scripts/DinoController.cs
@@ -7,20 +7,53 @@ public class DinoController : MonoBehaviour
 {
     public float zMoveSpeed;
     public float xMoveSpeed;
+    // 구체의 중심이될위치
+    public Vector3 sphereCenter;
+    // 구체의 반지름
+    public float sphereRadius = 0.5f;
     void Start()
     {
     }
     void Update()
     {
-        transform.position += Vector3.forward * Time.deltaTime * zMoveSpeed; // 매 프레임마다 객체를 +x 방향으로 1유닛씩 이동
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(-xMoveSpeed * Time.deltaTime, 0, 0); 
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(xMoveSpeed * Time.deltaTime, 0, 0);
-        }
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -3.9f, 3.9f), transform.position.y, transform.position.z);
+        DinoMove();
+     
     }
-}
+
+        void DinoMove()
+        {
+            {
+                // Z축으로계속앞으로이동
+                transform.position += Vector3.forward * Time.deltaTime * zMoveSpeed;
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    transform.Translate(-xMoveSpeed * Time.deltaTime, 0, 0);
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.Translate(xMoveSpeed * Time.deltaTime, 0, 0);
+                }
+                transform.position = new Vector3(Mathf.Clamp(transform.position.x, -3.9f, 3.9f), transform.position.y, transform.position.z);
+            }
+
+
+            void DoorCheck()
+            {
+                // 구체 영역 내의Collider들을 감지
+                Collider[] hitColliders = Physics.OverlapSphere(transform.position + sphereCenter, sphereRadius);
+                // 감지된 Collider들 처리
+                foreach (Collider doors in hitColliders)
+                {
+                    Debug.Log("감지된 오브젝트: " + doors.gameObject.name);
+                }
+            }
+            // 구체 영역을 Gizmo로 시각화
+            void OnDrawGizmos()
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(transform.position + sphereCenter, sphereRadius);
+            }
+        }
+    }
+
+ 
--- Assets/02.Scripts/SelectDoors.cs
+++ Assets/02.Scripts/SelectDoors.cs
@@ -1,18 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SelectDoors : MonoBehaviour
 {
-    // Start is called before the first frame update
+    enum DoorType
+    {
+        Plus,
+        Minus,
+        Times,
+        Division
+    }
+    public SpriteRenderer rightDoorSpriteRD;  // 오른쪽 문의 색을 관리할 변수
+    public SpriteRenderer leftDoorSpriteRD;   // 왼쪽 문의 색을 관리할 변수
+    public TextMeshPro rightDoorText;   //오른쪽 문의 Text를 관리할 변수
+    public TextMeshPro leftDoorText;    //왼쪽 문의 Text를 관리할 변수
+    [SerializeField]
+    private DoorType rightDoorType;     // 오른쪽 문의 상태를 관리할변수
+    public int rightDoorNumber;
+    // 오른쪽 문에서계산될숫자변수
+    [SerializeField]
+    private DoorType leftDoorType;      // 왼쪽 문의 상태를관리할변수
+    public int leftDoorNumber;
+    // 왼쪽 문에서계산될숫자변수
+    public Color goodColor;             // 좋은쪽문의색
+    public Color badColor;              // 나쁜쪽문의색
     void Start()
     {
-        
+        SettingDoors();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void SettingDoors()
+    {
+        // 오른쪽 문 세팅
+        if (rightDoorType.Equals(DoorType.Plus))    // 더하기
+        {
+            rightDoorSpriteRD.color = goodColor;
+            rightDoorText.text = "+" + rightDoorNumber;
+        }
+        else if (rightDoorType.Equals(DoorType.Minus)) // 빼기
+        {
+            rightDoorSpriteRD.color = badColor;
+            rightDoorText.text = "-" + rightDoorNumber;
+        }
+        else if (rightDoorType.Equals(DoorType.Times)) // 곱하기
+        {
+            rightDoorSpriteRD.color = goodColor;
+            rightDoorText.text = "x" + rightDoorNumber;
+        }
+        else if (rightDoorType.Equals(DoorType.Division)) // 나누기
+        {
+            rightDoorSpriteRD.color = badColor;
+            rightDoorText.text = "/ " + rightDoorNumber;
+        }
+
+        // 왼쪽 문세팅
+        if (leftDoorType.Equals(DoorType.Plus))  // 더하기
+        {
+            leftDoorSpriteRD.color = goodColor;
+            leftDoorText.text = "+" + leftDoorNumber;
+        }
+        else if (leftDoorType.Equals(DoorType.Minus)) // 빼기
+        {
+            leftDoorSpriteRD.color = badColor;
+            leftDoorText.text = "-" + leftDoorNumber;
+        }
+        else if (leftDoorType.Equals(DoorType.Times)) // 곱하기
+        {
+            leftDoorSpriteRD.color = goodColor;
+            leftDoorText.text = "x" + leftDoorNumber;
+        }
+        else if (leftDoorType.Equals(DoorType.Division)) // 나누기
+        {
+            leftDoorSpriteRD.color = badColor;
+            leftDoorText.text = "/ " + leftDoorNumber;
+        }
+    }
+
 }
+
+
+
